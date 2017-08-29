efd30ace6fcbb80eb354750c0fea393689c23098
Thomas Lee <thomasjlee@live.com>
2017-08-29 17:04:10 -0500

    change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170802163800) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "high_scores", force: :cascade do |t|
     t.string   "name"
     t.integer  "wpm"
